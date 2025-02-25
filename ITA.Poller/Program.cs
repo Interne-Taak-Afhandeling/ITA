0b2755118243649bbb89b1cc272038dc59ebbee0
Henok Solomon <henok@icatt.nl>
2025-02-25 19:05:58 +0300

    Remove Polly dependencies and reorder imports
    Removed `Microsoft.Extensions.Http.Polly` and `Polly` package references from `ITA.Poller.csproj`. Adjusted import statements in `Program.cs` by removing `using Polly;` and reordering the remaining imports. Added a Byte Order Mark (BOM) character at the beginning of `ITA.Poller.csproj`.

--- ITA.Poller/Program.cs
+++ ITA.Poller/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Polly;
+using Microsoft.Extensions.Logging; 
 using Serilog;
 using ITA.Poller.Services;
 
