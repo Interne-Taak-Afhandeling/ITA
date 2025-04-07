f4991ba3f5ef5e9f4ad5f464af5642045b3fb19e
githubjimmeicatt <jimme@icatt.nl>
2025-04-07 16:33:34 +0200

    remove testdata migrations and add docker profile (for easy use of connectionsstring when using EF)

--- InterneTaakAfhandeling.Web.Server/Data/ApplicationDbContext.cs
+++ InterneTaakAfhandeling.Web.Server/Data/ApplicationDbContext.cs
@@ -8,24 +8,9 @@ namespace InterneTaakAfhandeling.Web.Server.Data
         {
         }
 
-        public DbSet<TestItem> TestItems { get; set; }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<TestItem>().HasData(
-                new TestItem { Id = 1, Name = "Test Item 1", Description = "This is test item 1" },
-                new TestItem { Id = 2, Name = "Test Item 2", Description = "This is test item 2" },
-                new TestItem { Id = 3, Name = "Test Item 3", Description = "This is test item 3" }
-            );
         }
-    }
-
-    public class TestItem
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
     }
 }
