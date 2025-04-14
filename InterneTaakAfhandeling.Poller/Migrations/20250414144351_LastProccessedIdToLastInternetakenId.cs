fd4818ab5b81cd5d35cd0554d1b8e9b66a81db51
Henok Solomon <henok@icatt.nl>
2025-04-14 18:07:23 +0300

    Rename column in InternetakenNotifierStates table

---
+++ InterneTaakAfhandeling.Poller/Migrations/20250414144351_LastProccessedIdToLastInternetakenId.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InterneTaakAfhandeling.Poller.Migrations
+{
+    /// <inheritdoc />
+    public partial class LastProccessedIdToLastInternetakenId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastProcessedId",
+                table: "InternetakenNotifierStates");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LastInternetakenId",
+                table: "InternetakenNotifierStates",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastInternetakenId",
+                table: "InternetakenNotifierStates");
+
+            migrationBuilder.AddColumn<long>(
+                name: "LastProcessedId",
+                table: "InternetakenNotifierStates",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}
