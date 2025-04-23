7621acfd82d4d35cd6f8bcac04c4717357cbbff5
Henok Solomon <henok@icatt.nl>
2025-04-23 15:57:51 +0300

    Add timestamps to InternetakenNotifierState and migration

--- InterneTaakAfhandeling.Poller/Data/Entities/InternetakenNotifierState.cs
+++ InterneTaakAfhandeling.Poller/Data/Entities/InternetakenNotifierState.cs
@@ -3,11 +3,15 @@
 
 public class InternetakenNotifierState
 {
-    public Guid Id { get; set; } 
+    public Guid Id { get; set; }
     public Guid LastInternetakenId { get; set; }
+    public DateTimeOffset LastInternetakenToegewezenOp { get; set; }
+    public DateTimeOffset LastRunAt { get; set; }
+
+    // Timestamp indicating when the last poller successfully processed all the internetaken.
     public DateTimeOffset? LastSuccessAt { get; set; }
     public int FailureCount { get; set; }
     public bool IsRunning { get; set; }
     public DateTimeOffset UpdatedAt { get; set; }
     public string? Remark { get; set; }
-}
+} 
---
+++ InterneTaakAfhandeling.Poller/Migrations/20250423125624_AddedLastInternetakenToegewezenOpAndLastRunAt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InterneTaakAfhandeling.Poller.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedLastInternetakenToegewezenOpAndLastRunAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastInternetakenToegewezenOp",
+                table: "InternetakenNotifierStates",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastRunAt",
+                table: "InternetakenNotifierStates",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastInternetakenToegewezenOp",
+                table: "InternetakenNotifierStates");
+
+            migrationBuilder.DropColumn(
+                name: "LastRunAt",
+                table: "InternetakenNotifierStates");
+        }
+    }
+}
