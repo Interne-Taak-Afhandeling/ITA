b953a181edf53e62d87d1ddd5d539187ed470fac
Henok Solomon <henok@icatt.nl>
2025-04-25 19:30:53 +0300

    Handle undefined cases in dateUtils and DashboardView

--- InterneTaakAfhandeling.Web.Client/src/utils/dateUtils.ts
+++ InterneTaakAfhandeling.Web.Client/src/utils/dateUtils.ts
@@ -1,7 +1,10 @@
-export const formatDate = (dateString: string): string => {
-    return new Date(dateString).toLocaleString("nl-NL", {
-      day: "2-digit",
-      month: "2-digit",
-      year: "numeric",
-    });
+export const formatDate = (dateString: string | undefined): string => {  
+   if (!dateString) {  
+       return "N/A";  
+   }  
+   return new Date(dateString).toLocaleString("nl-NL", {  
+       day: "2-digit",  
+       month: "2-digit",  
+       year: "numeric",  
+   });  
 };
