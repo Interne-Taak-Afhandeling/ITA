13cd394a8921a4ae0fb0181acec1b35c641ae00b
Felix Cornelissen <felix.cornelissen@icatt.nl>
2025-05-07 17:29:09 +0200

    cleanup

--- InterneTaakAfhandeling.Common/Services/OpenklantApi/KnownMedewerkerIdentificators.cs
+++ InterneTaakAfhandeling.Common/Services/OpenklantApi/KnownMedewerkerIdentificators.cs
@@ -2,30 +2,31 @@
 {
     public class KnownMedewerkerIdentificators
     {
-        public static readonly KnownMedewerkerIdentificator EmailFromEntraId = new()
+        public static readonly KnownMedewerkerIdentificators EmailFromEntraId = new()
         {
             CodeObjecttype = "mdw",
             CodeRegister = "msei",
             CodeSoortObjectId = "email"
         };
 
-        public static readonly KnownMedewerkerIdentificator EmailHandmatig = new()
+        public static readonly KnownMedewerkerIdentificators EmailHandmatig = new()
         {
             CodeObjecttype = "mdw",
             CodeRegister = "handmatig",
             CodeSoortObjectId = "email"
         };
 
-        public static readonly KnownMedewerkerIdentificator IdFromObjectRegistration = new()
+        public static readonly KnownMedewerkerIdentificators IdFromObjectRegistration = new()
         {
             CodeObjecttype = "mdw",
             CodeRegister = "obj",
             CodeSoortObjectId = "idf"
         };
-    }
 
-    public class KnownMedewerkerIdentificator
-    {
+        private KnownMedewerkerIdentificators()
+        {
+        }
+
         public required string CodeSoortObjectId { get; init; }
         public required string CodeObjecttype { get; init; }
         public required string CodeRegister { get; init; }
