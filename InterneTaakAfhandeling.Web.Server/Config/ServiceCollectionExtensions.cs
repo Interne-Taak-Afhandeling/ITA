5cdc8ac95c98d9a958013177e585775b7e12bc62
Henok Solomon <henok@icatt.nl>
2025-05-22 16:21:51 +0300

    duplicate service collection removed for ObjectregisterMedewerkerIdClaimType

--- InterneTaakAfhandeling.Web.Server/Config/ServiceCollectionExtensions.cs
+++ InterneTaakAfhandeling.Web.Server/Config/ServiceCollectionExtensions.cs
@@ -31,8 +31,7 @@ namespace InterneTaakAfhandeling.Web.Server.Config
               options.NameClaimType = configuration["OIDC_NAME_CLAIM_TYPE"];
               options.RoleClaimType = configuration["OIDC_ROLE_CLAIM_TYPE"];
               options.ObjectregisterMedewerkerIdClaimType = configuration["OIDC_OBJECTREGISTER_MEDEWERKER_ID_CLAIM_TYPE"];
-              options.EmailClaimType = configuration["OIDC_EMAIL_CLAIM_TYPE"];
-              options.ObjectregisterMedewerkerIdClaimType= configuration["OIDC_OBJECTREGISTER_MEDEWERKER_ID_CLAIM_TYPE"];
+              options.EmailClaimType = configuration["OIDC_EMAIL_CLAIM_TYPE"]; 
           });
 
              
