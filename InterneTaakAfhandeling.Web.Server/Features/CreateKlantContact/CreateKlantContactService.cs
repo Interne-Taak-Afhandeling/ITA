1f6c15cba33ed5f4b932308e3ab42d2177d70bbb
mark stoker <mark@icatt.nl>
2025-05-26 14:32:13 +0200

    merge from main

--- InterneTaakAfhandeling.Web.Server/Features/CreateKlantContact/CreateKlantContactService.cs
+++ InterneTaakAfhandeling.Web.Server/Features/CreateKlantContact/CreateKlantContactService.cs
@@ -1,9 +1,11 @@
 ﻿using InterneTaakAfhandeling.Common.Helpers;
+using InterneTaakAfhandeling.Common.Services.OpenklantApi;
+using InterneTaakAfhandeling.Common.Services.OpenklantApi.Models;
 using InterneTaakAfhandeling.Common.Services.OpenKlantApi;
 using InterneTaakAfhandeling.Common.Services.OpenKlantApi.Models;
 using InterneTaakAfhandeling.Web.Server.Middleware;
 using InterneTaakAfhandeling.Web.Server.Services;
-using System;
+
 
 namespace InterneTaakAfhandeling.Web.Server.Features.CreateKlantContact
 {
