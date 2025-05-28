3397705f6fd575d1cc946b43a7e54a8817ef8ded
Felix Cornelissen <felix.cornelissen@icatt.nl>
2025-05-28 15:24:03 +0200

    fix color and spacing

--- InterneTaakAfhandeling.Web.Client/src/components/UserInfo.vue
+++ InterneTaakAfhandeling.Web.Client/src/components/UserInfo.vue
@@ -1,15 +1,10 @@
 <template>
   <div class="user-info-component">
     <div v-if="isAuthenticated" class="user-info">
-      <span class="user-name">{{ user?.name }}</span>
-      <button
-        class="utrecht-button utrecht-button--primary utrecht-button--action-button"
-        @click="logout"
-        :disabled="isLoading"
-      >
-        <span v-if="isLoading">Bezig...</span>
-        <span v-else>Uitloggen</span>
-      </button>
+      <span class="user-name utrecht-nav-list__item">{{ user?.name }}</span>
+      <a href="/api/logoff" class="utrecht-link utrecht-link--html-a utrecht-nav-list__link">
+        Uitloggen
+      </a>
     </div>
   </div>
 </template>
@@ -19,11 +14,7 @@ import { useAuthStore } from "@/stores/auth";
 import { storeToRefs } from "pinia";
 
 const authStore = useAuthStore();
-const { user, isAuthenticated, isLoading } = storeToRefs(authStore);
-
-function logout() {
-  authStore.logout();
-}
+const { user, isAuthenticated } = storeToRefs(authStore);
 </script>
 
 <style scoped>
@@ -39,6 +30,6 @@ function logout() {
 }
 
 .user-name {
-  font-weight: 500;
+  inline-size: max-content;
 }
 </style>
