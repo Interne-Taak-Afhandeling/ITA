0de66301c6ea903a0c98aa1106fbe5879d5813e6
Henok Solomon <henok@icatt.nl>
2025-07-10 17:57:52 +0300

    added api client impl

--- InterneTaakAfhandeling.Web.Client/src/services/internetakenService.ts
+++ InterneTaakAfhandeling.Web.Client/src/services/internetakenService.ts
@@ -1,8 +1,12 @@
 import { get } from "@/utils/fetchWrapper";
+import { post } from "@/utils/fetchWrapper";
 import type { Internetaken } from "@/types/internetaken";
 
 export const internetakenService = {
   getInternetaak: (internetaakNummer: string): Promise<Internetaken> => {
     return get<Internetaken>(`/api/internetaken/${internetaakNummer}`);
+  },
+  addNoteToInternetaak: (internetaakId: string, note: string) => {
+    return post(`/api/internetaken/${internetaakId}/add-note`, { note });
   }
 };
--- InterneTaakAfhandeling.Web.Server/Services/LogboekService/LogboekService.cs
+++ InterneTaakAfhandeling.Web.Server/Services/LogboekService/LogboekService.cs
@@ -155,6 +155,7 @@ public class LogboekService(IObjectApiClient objectenApiClient, IOpenKlantApiCli
         {
             Datum = DateTime.Now,
             Type = knownContactAction.Type,
+            Notitie = knownContactAction.Notitie,
             Omschrijving = knownContactAction.Description,
             Actor = knownContactAction.Actor,
             HeeftBetrekkingOp = knownContactAction.HeeftBetrekkingOp != null
