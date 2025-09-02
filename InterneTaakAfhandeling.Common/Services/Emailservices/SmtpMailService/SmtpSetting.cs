1ec23d1fa0ad0cef9d5773c86bac9ec20e3b18c8
Youri Dudock <youri@icatt.nl>
2025-09-02 11:12:12 +0200

    smpt remove whitelining

--- InterneTaakAfhandeling.Common/Services/Emailservices/SmtpMailService/SmtpSetting.cs
+++ InterneTaakAfhandeling.Common/Services/Emailservices/SmtpMailService/SmtpSetting.cs
@@ -2,8 +2,6 @@
 {
     public class SmtpSettings
     {
-
-
         public string Host { get; set; } = string.Empty;
         public int Port { get; set; }
         public string Username { get; set; } = string.Empty;
